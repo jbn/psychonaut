d6fdd184813ca59333bf2a1237b5b430bbc37ca9
generativist <jbn@abreka.com>
2023-05-13 10:30:47 -0700

    Make config more robust

--- psychonaut/cli/config.py
+++ psychonaut/cli/config.py
@@ -1,6 +1,6 @@
 import click
 from psychonaut.client.credentials import write_homedir_creds
-from .util import print_error_and_fail
+from .util import clean_handle, print_error_and_fail
 from .group import cli
 
 
@@ -8,6 +8,7 @@ from .group import cli
 @click.argument("handle")
 @click.option("--allow-overwrite", is_flag=True, default=False)
 def save_login(handle: str, allow_overwrite: bool):
+    handle = clean_handle(handle)
     try:
         with write_homedir_creds(handle, allow_overwrite) as f:
             f(click.prompt(f"Enter password for {handle}", hide_input=True))
